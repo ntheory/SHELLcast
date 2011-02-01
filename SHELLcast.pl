9df0cc48287cc5fd819a454ded3bd67b799f8424
ntheory <ntheory@entanglement.net>
2011-01-31 23:57:54 -0800

    Moved very old code over from SourceForge (https://sourceforge.net/projects/shellcast/).  This project was originally created on 2003-04-02.  This release is from 2003-04-24.

---
+++ SHELLcast.pl
@@ -0,0 +1,98 @@
+#!/usr/local/bin/perl -w
+
+# SHELLcast
+# by ntheory
+#
+# March 22nd, 2003 - Started development.
+# April 3rd, 2003  - Posted to SourceForge.
+
+# -- <BEHOLD> THE GPL! --
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# -- </BEHOLD> --
+
+require 5.002;
+use IO::Socket;
+use IO::Select;
+use Term::ReadKey;
+
+# Create a socket to listen to a port
+my $Listen = IO::Socket::INET->new(Proto => 'tcp',
+				   LocalPort => 2323,
+				   Listen => 1,
+				   Reuse => 1) or die $!;
+
+# To start with, $select contains only the socket we're listening on
+my $Select = IO::Select->new ($Listen);
+
+# Autoflush STDOUT.
+$| = 1;
+
+# Read raw data from STDIN
+ReadMode 4;
+
+my $Delay = 0.10;
+my $Character;
+
+my @Ready;
+
+# Tell the client we'll echo their characters (but we won't).  This
+# is just like entering a password on a telnet session.  This way
+# the client doesn't echo the characters themselves and make it look
+# like they're successfully typing over us.
+my $TelnetWillEcho = chr (0xFF) . chr (0xFB) . chr (0x01);
+
+# wait until there's something to do 
+while (1) {
+  if (($Character = ReadKey ($Delay, STDIN)) ||
+      (@Ready = $Select->can_read ($Delay))) {
+    my $Socket;
+
+    # Handle each socket that's ready
+    for $Socket (@Ready) {
+      if ($Socket == $Listen) {
+        # Accepts a new connection.
+        my $New = $Listen->accept;
+        $Select->add ($New);
+        print $New->fileno . ": connected\n\r";
+	$New->send ($TelnetWillEcho);
+      }
+    }
+
+    if (defined ($Character)) {
+      # Send this data to our clients.
+      if ($Character eq "\n") {
+	# Slap an extra CR on there in case the incoming program doesn't send
+	# CRLF (just LF).
+        $Character .= "\r";
+      }
+
+      print $Character;
+
+      for $Socket ($Select->handles) {
+        # Skip the listening socket.
+        next if ($Socket == $Listen);
+
+        if ($Socket->peername) {
+          $Socket->send ($Character);
+	}
+	else {
+          print $Socket->fileno . ": disconnected\n\r";		
+          $Select->remove ($Socket);
+          $Socket->close;
+        }
+      }
+    }
+  }
+}
